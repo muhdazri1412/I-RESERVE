30498b1a5e2e83ad8a543c70a1f771c2ebae81de
owish <owishaa@gmail.com>
2021-11-19 03:42:27 +0800

    Create validation.php
    add validation contoroler

---
+++ validation.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+
+
+$con = mysqli_connect('localhost','root','');
+
+mysqli_select_db($con, 'userregistration' );
+
+$matricno = $_POST['matricno'];
+$pass = $_POST['password'];
+
+$s = "select * from userlogin where matricno= '$matricno' && password = '$pass'";
+
+$result = mysqli_query($con,$s);
+
+$num = mysqli_num_rows($result);
+
+if($num==1){
+  
+    header('location:mainpage.php');
+
+}else{
+
+    header('location:login.php');
+
+}
+
+ ?>
